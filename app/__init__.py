6f8fbeaed47bbf79c0fd3f5fb82fe4544f247d4f
Daniel Battick <danielbattick@gmail.com>
2020-02-16 00:43:14 -0500

    Lab complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '530dab7079cef3a285e72baf73af099a'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = '465'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
+
+mail = Mail(app)
+csrf = CSRFProtect(app) # Used to protect the form with a CSRF token. Step 3 of lab, instruction 3.
 from app import views
--- app/views.py
+++ app/views.py
@@ -6,7 +6,10 @@ This file creates your application.
 """
 
 from app import app
+from app import mail
+from app.forms import ContactForm
 from flask import render_template, request, redirect, url_for, flash
+from flask_mail import Message
 
 
 ###
@@ -18,11 +21,30 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            name = form.name.data
+            email = form.email.data
+            subject = form.subject.data
+            message = form.message.data
+
+            msg = Message(subject, sender=(name,email),recipients=["to@example.com"])
+            msg.body = message
+            mail.send(msg)
+
+            flash("You have successfully filled out the form!", 'success')
+            return redirect(url_for('home'))
+        flash_errors(form)        
+    return render_template('contact.html', form=form)
+
 
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Daniel Battick")
 
 
 ###
